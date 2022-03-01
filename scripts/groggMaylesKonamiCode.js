33212460bd556e0c64abc0705c9e69fcf4f914a8
Ryoley <chevrierpaul.pc@gmail.com>
2022-03-01 15:49:20 +0100

    make the website more responsive

---
+++ scripts/groggMaylesKonamiCode.js
@@ -0,0 +1,17 @@
+function groggMaylesKonamiCode(response) {
+    let input = '';
+    const key = '71827971';
+    document.addEventListener('keydown', function (e) {
+        input += ("" + e.keyCode);
+        if (input === key) {
+            return response();
+        }
+        if (!key.indexOf(input)) return;
+        input = ("" + e.keyCode);
+    });
+}
+
+groggMaylesKonamiCode(function () {
+    const audio = new Audio('../audio/grogg-mayles.mp3.mp3');
+    audio.play();
+})
